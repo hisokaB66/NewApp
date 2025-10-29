d0691482c2d5d24c30c2efa3b3bd6475f57417fc
sj <joolee4570@navaer.com>
2025-10-29 16:06:23 +0900

    update week02

--- week02/src/main/java/com/example/week02/MainActivity.kt
+++ week02/src/main/java/com/example/week02/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.week02.ui.theme.NewAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    HomeScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen() {
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Compose Coffee")
+        Image(
+            contentDescription = "My Image",
+            painter = painterResource(id = R.drawable.compose),
+            modifier = Modifier
+                .size(size = 100.dp)
+                .padding(vertical = 16.dp)
+        )
+        Text(text = "위치는 우송대 정문 앞")
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NewAppTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }

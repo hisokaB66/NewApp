c7e1ae6d8194831b026a23277e053140e2d016a9
sj <joolee4570@navaer.com>
2025-10-29 19:06:23 +0900

    week03

--- week02/src/main/java/com/example/week02/MainActivity.kt
+++ week02/src/main/java/com/example/week02/MainActivity.kt
@@ -4,18 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.week02.ui.theme.NewAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,33 +20,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewAppTheme {
-                    HomeScreen()
+                Main()
             }
         }
     }
 }
 
 @Composable
-fun HomeScreen() {
+fun Main() {
+    Column(modifier = Modifier.padding(16.dp)) {
 
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Compose Coffee")
-        Image(
-            contentDescription = "My Image",
-            painter = painterResource(id = R.drawable.compose),
-            modifier = Modifier
-                .size(size = 100.dp)
-                .padding(vertical = 16.dp)
-        )
-        Text(text = "위치는 우송대 정문 앞")
+        Text("1부터 5까지:")
+        for (i in 1..5) {
+            Text("- $i")
+        }
 
+        Text("\n1부터 10까지 홀수:")
+        for (i in 1..10 step 2) {
+            Text("- $i")
+        }
+
+        Text("\n5부터 1까지 감소:")
+        for (i in 5 downTo 1) {
+            Text("- $i")
+        }
+
+        Text("\n1부터 5 전까지 (until):")
+        for (i in 1 until 5) {
+            Text("- $i")
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     NewAppTheme {
-        HomeScreen()
+        Main()
     }
 }
